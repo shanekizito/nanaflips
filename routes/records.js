8ccb212fe7f51908fbba78dc147b1f22dbcc4a36
SHARK-FI <79025747+shanekizito@users.noreply.github.com>
2021-12-29 02:30:12 +0300

    Update records.js

--- routes/records.js
+++ routes/records.js
@@ -35,7 +35,7 @@ router.route('/').get((req, res) => {
     var asset_array=[];
 
 
-    for (var b=2000000; b<20000000;b=b+300){
+    for (var b=147473647; b<147483647;b=b+300){
   
    var offset=b;
    console.log("offset:"+offset);
