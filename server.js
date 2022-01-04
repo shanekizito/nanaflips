c53cc49e07d2b2635cce19e9507c9520fa5c9c28
SHARK-FI <79025747+shanekizito@users.noreply.github.com>
2022-01-04 23:27:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require("express");
 var bodyParser = require('body-parser');
 const app = express();
+
 const cors = require("cors");
-const fetch = require('node-fetch');
+
+
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
+
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyParser.json({limit: '50mb'}));
+
 app.use(express.json());
 //app.use(express.bodyParser({limit: '50mb'}));
-app.use(require('./routes/records'));
 
 // get driver connection
 const dbo = require("./db/conn");
 
-app.use(express.json());
-//app.use(express.bodyParser({limit: '50mb'}));
-
-
+app.use(require('./routes/records'));
+ 
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
@@ -27,11 +28,3 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
-
-
-
-
-
-
-
-
